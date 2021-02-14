9fb39443753494e7cc166b3e1f05b61e92bc7d5f
Szórádi Géza <szoradigeza@gmail.com>
2021-02-14 13:46:52 +0100

    table and draw

---
+++ app.js
@@ -0,0 +1,39 @@
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 40;
+
+function setup() {
+    createCanvas(600, 600)
+    cols = width / resolution;
+    rows = height / resolution;
+    grid = make2DArray(cols, rows);
+    for(let i = 0; i< cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
+    }
+}
+  
+function draw() {
+    background(0);
+    for(let i = 0; i< cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if(grid[i][j] == 1) {
+                fill(255);
+                stroke(0);
+                rect(x, y, resolution - 1, resolution - 1);
+            }
+        }
+    }
+}
